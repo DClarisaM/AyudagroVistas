57cf57dfbe5a6cd9b2fb0ba3fa8e327445f44a1c
Andrescruzxr <andrescruzx26@gmail.com>
2023-05-10 19:29:35 -0500

    vistas comentadas vistas innecesarias

--- src/pages/Archivos/ListarArchivo.vue
+++ src/pages/Archivos/ListarArchivo.vue
@@ -2,23 +2,24 @@
     <div>
       
 <drop-down class="nav-item" title="Admin"  icon="ti-linux">
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'categorias'}">Administrar Categorias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'subcategorias'}">Administrar SubCategorias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'usuario'}">Ver Usuario</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerencias'}">Ver Sugerencias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'valorR'}">Ver ValorR</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'pregunta'}">Ver Pregunta</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'respuesta'}">Ver Respuesta</router-link></a>
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'categorias'}">Administrar Categorias</router-link></a> -->
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'subcategorias'}">Administrar SubCategorias</router-link></a> -->
+  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'usuario'}">Ver Usuarios</router-link></a>
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerencias'}">Ver Sugerencias</router-link></a> -->
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'valorR'}">Ver ValorR</router-link></a> -->
+  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'pregunta'}">Ver Pregunta</router-link></a> 
+   <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'respuesta'}">Ver Respuesta</router-link></a> 
   <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'rol'}">Gestionar Rol</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'ListaPreguntas'}">Preguntas</router-link></a>
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'ListaPreguntas'}">Preguntas</router-link></a> -->
 
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerenciaArchivo'}">mostrar Sugerencias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'preguntaArchivo'}">Listar Pregunta Archivo</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'MisPreguntas'}">Mis Preguntas</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'SinRespuesta'}">Sin Respuesta</router-link></a>
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerenciaArchivo'}">mostrar Sugerencias</router-link></a> -->
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'preguntaArchivo'}">Listar Pregunta Archivo</router-link></a> -->
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'MisPreguntas'}">Mis Preguntas</router-link></a> -->
+  <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'SinRespuesta'}">Sin Respuesta</router-link></a> -->
  
 
 </drop-down>
+
         
       
              <!-- boton agregar -->
@@ -32,6 +33,8 @@
     </router-link>
     </div>
     <!-- aqui termin el boton -->
+
+    
   
       <div class="col-12">
       <card class="card-plain">
@@ -65,12 +68,14 @@
   
                 </td>
               </tr>
+              
   
             </tbody>
           </table>
           
         </div>
       </card>
+      
     </div>
   </div>
   </template>
--- src/pages/Roles/GestionarRol.vue
+++ src/pages/Roles/GestionarRol.vue
@@ -1,77 +1,82 @@
 <template>
-  <div>    
-  <drop-down class="nav-item" title="Admin"  icon="ti-linux">
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'categorias'}">Administrar Categorias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'subcategorias'}">Administrar SubCategorias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'usuario'}">Ver Usuario</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerencias'}">Ver Sugerencias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'valorR'}">Ver ValorR</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'pregunta'}">Ver Pregunta</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'respuesta'}">Ver Respuesta</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'rol'}">Gestionar Rol</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'ListaPreguntas'}">Preguntas</router-link></a>
+  <div>
+    <drop-down class="nav-item" title="Admin" icon="ti-linux">
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'categorias'}">Administrar Categorias</router-link></a> -->
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'subcategorias'}">Administrar SubCategorias</router-link></a> -->
+      <a class="dropdown-item"><router-link class="text-center form-text" :to="{ name: 'usuario' }">Ver
+          Usuarios</router-link></a>
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerencias'}">Ver Sugerencias</router-link></a> -->
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'valorR'}">Ver ValorR</router-link></a> -->
+      <a class="dropdown-item"><router-link class="text-center form-text" :to="{ name: 'pregunta' }">Ver
+          Pregunta</router-link></a>
+      <a class="dropdown-item"><router-link class="text-center form-text" :to="{ name: 'respuesta' }">Ver
+          Respuesta</router-link></a>
+      <a class="dropdown-item"><router-link class="text-center form-text" :to="{ name: 'rol' }">Gestionar
+          Rol</router-link></a>
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'ListaPreguntas'}">Preguntas</router-link></a> -->
 
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerenciaArchivo'}">mostrar Sugerencias</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'preguntaArchivo'}">Listar Pregunta Archivo</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'MisPreguntas'}">Mis Preguntas</router-link></a>
-  <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'SinRespuesta'}">Sin Respuesta</router-link></a>
- 
-
-</drop-down>
-  <div class="row">
-           <!-- boton agregar -->
-  <div class="col-8 offset-2">
-    <router-link
-    class="btn"
-    :to="{
-      name: 'Registrarrol'
-    }">
-      Agregar
-  </router-link>
-  </div>
-  <!-- aqui termin el boton -->
-
-    <div class="col-12">
-    <card class="card-plain">
-      <div class="table-full-width table-responsive">
-        <table class="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="rol in listaRol">
-              <th scope="row">{{ rol.id_rol }}</th>
-              <td>{{ rol.nombre }}</td>
-              <td>
-                <router-link class="btn bg-primary text-white" :to="{name: 'Verrol', params: {idRol: rol.id_rol}
-                  }">
-                    <i class="fas fa-eye"></i>
-                  </router-link>
-                  <router-link class="btn bg-success text-white" :to="{ name: 'Editarrol', 
-                  // params: { idRol: archivo.id_rol } 
-                  }">
-                    <i class="fas fa-edit"></i>
-                  </router-link>
-                
-                  <button class="btn  bg-danger text-white" @click="eliminar()">
-                    <i class="fas fa-trash-alt"></i>
-                  </button>
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'sugerenciaArchivo'}">mostrar Sugerencias</router-link></a> -->
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'preguntaArchivo'}">Listar Pregunta Archivo</router-link></a> -->
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'MisPreguntas'}">Mis Preguntas</router-link></a> -->
+      <!-- <a class="dropdown-item"><router-link class="text-center form-text" :to="{name:'SinRespuesta'}">Sin Respuesta</router-link></a> -->
 
 
-              </td>
-            </tr>
-
-          </tbody>
-        </table>
-        
+    </drop-down>
+    <div class="row">
+      <!-- boton agregar -->
+      <div class="col-8 offset-2">
+        <router-link class="btn" :to="{
+          name: 'Registrarrol'
+        }">
+          Agregar
+        </router-link>
       </div>
-    </card>
-  </div>
-  </div>
+      <!-- aqui termin el boton -->
+
+      <div class="col-12">
+        <card class="card-plain">
+          <div class="table-full-width table-responsive">
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="rol in listaRol">
+                  <th scope="row">{{ rol.id_rol }}</th>
+                  <td>{{ rol.nombre }}</td>
+                  <td>
+                    <router-link class="btn bg-primary text-white" :to="{
+                      name: 'Verrol', params: { idRol: rol.id_rol }
+                    }">
+                      <i class="fas fa-eye"></i>
+                    </router-link>
+                    <router-link class="btn bg-success text-white" :to="{
+                      name: 'Editarrol',
+                      // params: { idRol: archivo.id_rol } 
+                    }">
+                      <i class="fas fa-edit"></i>
+                    </router-link>
+
+                    <button class="btn  bg-danger text-white" @click="eliminar()">
+                      <i class="fas fa-trash-alt"></i>
+                    </button>
+                 
+
+
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+
+          </div>
+        </card>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -83,68 +88,69 @@ import Swal from 'sweetalert2'
 
 export default {
   components: {
- 
+
   },
   data() {
     return {
-      listaRol:[],
-     
+      listaRol: [],
+
     };
   },
-  mounted(){
+  mounted() {
     this.listarRol(),
-    eliminar()
+      eliminar(id)
   },
   methods: {
     listarRol() {
-       //alert("jahahsa")
+      //alert("jahahsa")
       axios.get("http://localhost:3000/listarRol")
-      .then((res) => {
+        .then((res) => {
 
-        this.listaRol = res.data
-        console.log(res.data);
-      })
-      .catch((err) => {
-        alert("error del servidor")
-      })
+          this.listaRol = res.data
+          console.log(res.data);
+        })
+        .catch((err) => {
+          alert("error del servidor")
+        })
     },
-    eliminar(){
+    eliminar() {
       // alert("hahaha")
       const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
 
-swalWithBootstrapButtons.fire({
-  title: 'Estas seguro ?',
-  text: "Esto se eliminara definitivo!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si, estoy seguro!',
-  cancelButtonText: 'No, cancelar!',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    axios.delete("http://localhost:3000/eliminarRol/5")
-    swalWithBootstrapButtons.fire(
-      'Eliminado!',
-      'Se elimino con exito.',
-      'success'
-    )
-  } else if (
-    /* Read more about handling dismissals below */
-    result.dismiss === Swal.DismissReason.cancel
-  ) {
-    swalWithBootstrapButtons.fire(
-      'Cancelado',
-      'No se eliminara :)',
-      'error'
-    )
-  }
-})
+      swalWithBootstrapButtons.fire({
+        title: 'Estas seguro ?',
+        text: "Esto se eliminara definitivo!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, estoy seguro!',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // id = getIdToDelete();
+          axios.delete(`http://localhost:3000/eliminarRol/10`)
+          swalWithBootstrapButtons.fire(
+            'Eliminado!',
+            'Se elimino con exito.',
+            'success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'No se eliminara :)',
+            'error'
+          )
+        }
+      })
 
     }
   }
